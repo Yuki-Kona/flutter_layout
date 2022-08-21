611fc09832c0974cbf3fd13fdd93d70586fc2718
Yuki-Kona <tnt0980@outlook.jp>
2022-08-21 20:44:36 +0900

    make2 commit

--- lib/app_bar/appbar_1.dart
+++ lib/app_bar/appbar_1.dart
@@ -22,49 +22,56 @@ class AppBar1 extends StatelessWidget{
         ),
       ),
       body:SizedBox(
-        child: Padding(
-          padding: const EdgeInsets.only(top:10.0,left: 20,right: 20),
-          child: Column(
-            children: [
+        child:ListView(
+          children:[ Padding(
+            padding: const EdgeInsets.only(top:10.0,left: 20,right: 20),
+            child: Column(
+              children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      label:  const Text('カラーコードでappBarの色を変える'),
+                      hintText: '現在のカラーコード${mainModelData.hash+mainModelData.colorCord}',
+                    ),
+                    maxLength: 6,
+                    inputFormatters:[
+                      FilteringTextInputFormatter.allow(RegExp(r'[0-9,a-f]')),
+                    ],
+                    onChanged: (colorNumber){
+                      if(colorNumber.length==6){
+                        mainModelData.colorChang(colorNumber);
+                      }
+                    }
+                  ),
                 TextField(
                   decoration: InputDecoration(
-                    label:  const Text('カラーコードでappBarの色を変える'),
-                    hintText: '現在のカラーコード${mainModelData.hash+mainModelData.colorCord}',
+                    labelText: 'appBarの高さを変更',
+                    hintText: '現在の高さ${mainModelData.height}',
                   ),
-                  maxLength: 6,
-                  inputFormatters:[
-                    FilteringTextInputFormatter.allow(RegExp(r'[0-9,a-f]')),
-                  ],
-                  onChanged: (colorNumber){
-                    if(colorNumber.length==6){
-                      mainModelData.colorChang(colorNumber);
+                  keyboardType: TextInputType.number,
+                  onSubmitted: (heightNumber){
+                    if(int.parse(heightNumber)<501 && int.parse(heightNumber)>19){
+                    mainModelData.heightChang(int.parse(heightNumber));
                     }
-                  }
+                  },
                 ),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'appBarの高さを変更',
-                  hintText: '現在の高さ${mainModelData.height}',
+                const SizedBox(
+                  height: 30,
                 ),
-                keyboardType: TextInputType.number,
-                onSubmitted: (heightNumber){
-                  if(int.parse(heightNumber)<501 && int.parse(heightNumber)>19){
-                  mainModelData.heightChang(int.parse(heightNumber));
-                  }
-                },
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Container(
-                width: 350,
-                height: 400,
-                child: PhotoView(
-                  imageProvider: const NetworkImage("http://www.visibone.com/color/hexagon_800.gif"),
+                SizedBox(
+                  width: 350,
+                  height: 400,
+                  child: Container(
+                    decoration: BoxDecoration(),
+                    clipBehavior: Clip.hardEdge,
+                    child: PhotoView(
+                      imageProvider: const AssetImage('img/img1.png'),
+                    ),
+                  ),
                 ),
-              )
-            ],
+              ],
+            ),
           ),
+        ]
         )
       ),
     );
